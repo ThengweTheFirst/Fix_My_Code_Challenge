fab28003e07e469e6f0f56e5bdb4cd57743841c6
ThengweTheFirst <109463825+ThengweTheFirst@users.noreply.github.com>
2023-02-24 00:40:53 +0200

    fizzbuzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,44 +1,35 @@
 #!/usr/bin/python3
-
+"""FizzBuzz
+"""
 import sys
 
-
-def fizzbuzz(n: int) -> str:
+def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
-    - For multiples of three print "Fizz" instead of the number and for
-      multiples of five print "Buzz".
-    - For numbers which are multiples of both three and five print "FizzBuzz".
+    - For multiples of three print "Fizz" instead of the number and for multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz"
     """
     if n < 1:
-        return ""
+        return
 
-    result = []
+    tmp_result = []
     for i in range(1, n + 1):
-        if i % 15 == 0:
-            result.append("FizzBuzz")
-        elif i % 3 == 0:
-            result.append("Fizz")
-        elif i % 5 == 0:
-            result.append("Buzz")
+        if (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
         else:
-            result.append(str(i))
-    return " ".join(result)
-
-
-def get_user_input() -> int:
-    """
-    Function to get user input.
-    """
-    if len(sys.argv) <= 1:
-        print("Missing number")
-        print("Usage: ./fizzbuzz.py <number>")
-        print("Example: ./fizzbuzz.py 89")
-        sys.exit(1)
-
-    return int(sys.argv[1])
-
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
 
 if __name__ == '__main__':
-    number = get_user_input()
-    print(fizzbuzz(number))
+    if len(sys.argv) <= 1:
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
+
+    number = int(sys.argv[1])
+    fizzbuzz(number)
